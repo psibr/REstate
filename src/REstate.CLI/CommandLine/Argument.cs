8953379bf33c4f2d80bb3d62038b9df90c7d5231
Ovan Crone <ovan.crone@skykick.com>
2017-12-20 21:07:04 -0800

    :tada: Initial REstate.CLI design idea

---
+++ src/REstate.CLI/CommandLine/Argument.cs
@@ -0,0 +1,151 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace REstate.CLI.CommandLine
+{
+    public abstract class Argument
+    {
+        internal Argument(ArgumentCommand command, IEnumerable<string> names, bool isOption, bool isRequired)
+        {
+            var nameArray = names.ToArray();
+            Command = command;
+            Name = nameArray.First();
+            Names = new ReadOnlyCollection<string>(nameArray);
+            IsOption = isOption;
+            IsRequired = isRequired;
+        }
+
+        public ArgumentCommand Command { get; private set; }
+
+        public string Name { get; private set; }
+
+        public ReadOnlyCollection<string> Names { get; private set; }
+
+        public string Help { get; set; }
+
+        public bool IsOption { get; private set; }
+
+        public bool IsParameter
+        {
+            get { return !IsOption; }
+        }
+
+        public bool IsSpecified { get; private set; }
+
+        public bool IsHidden { get; set; }
+
+        public bool IsRequired { get; private set; }
+        
+        public virtual bool IsList
+        {
+            get { return false; }
+        }
+
+        public object Value
+        {
+            get { return GetValue(); }
+        }
+
+        public object DefaultValue
+        {
+            get { return GetDefaultValue(); }
+        }
+
+        public bool IsActive
+        {
+            get { return Command == null || Command.IsActive; }
+        }
+
+        public abstract bool IsFlag { get; }
+
+        internal abstract object GetValue();
+
+        internal abstract object GetDefaultValue();
+
+        internal void MarkSpecified()
+        {
+            IsSpecified = true;
+        }
+
+        public string GetDisplayName()
+        {
+            return GetDisplayName(Name);
+        }
+
+        public IEnumerable<string> GetDisplayNames()
+        {
+            return Names.Select(GetDisplayName);
+        }
+
+        private string GetDisplayName(string name)
+        {
+            return IsOption ? GetOptionDisplayName(name) : GetParameterDisplayName(name);
+        }
+
+        private static string GetOptionDisplayName(string name)
+        {
+            var modifier = name.Length == 1 ? @"-" : @"--";
+            return modifier + name;
+        }
+
+        private static string GetParameterDisplayName(string name)
+        {
+            return @"<" + name + @">";
+        }
+
+        public virtual string GetDisplayValue()
+        {
+            return Value == null ? string.Empty : Value.ToString();
+        }
+
+        public override string ToString()
+        {
+            return GetDisplayName();
+        }
+    }
+
+    public sealed class Argument<T> : Argument
+    {
+        internal Argument(ArgumentCommand command, IEnumerable<string> names, T defaultValue, bool isRequired)
+            : base(command, names, true, isRequired)
+        {
+            Value = defaultValue;
+            Value = DefaultValue = defaultValue;
+        }
+
+        internal Argument(ArgumentCommand command, string name, T defaultValue)
+            : base(command, new[] { name }, false, true)
+        {
+            Value = defaultValue;
+            DefaultValue = defaultValue;
+        }
+
+        public new T Value { get; private set; }
+
+        public new T DefaultValue { get; private set; }
+
+        public override bool IsFlag
+        {
+            get { return typeof(T) == typeof(bool); }
+        }
+
+        internal override object GetValue()
+        {
+            return Value;
+        }
+
+        internal override object GetDefaultValue()
+        {
+            return DefaultValue;
+        }
+
+        internal void SetValue(T value)
+        {
+            Value = value;
+            MarkSpecified();
+        }
+    }
+}
---
+++ src/REstate.CLI/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using Grpc.Core;
+using REstate.CLI.CommandLine;
+using REstate.Remote;
+
+namespace REstate.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Commands.None;
+            var schematicName = string.Empty;
+            var remoteEndoint = "0.0.0.0:12345";
+            var stateType = typeof(string);
+            var inputType = typeof(string);
+
+            ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.DefineCommand("schematics", ref command, Commands.Schematics, "Opertions that can be performed on schematics.");
+                syntax.DefineParameter("name", ref schematicName, "The SchematicName.");
+                syntax.DefineOption("e|endpoint", ref remoteEndoint, "Endpoint of remote server. Defaults to 0.0.0.0:12345");
+                syntax.DefineOption("s|stateType", ref stateType, Type.GetType, false, "The CLR type name of the state.");
+                syntax.DefineOption("i|inputType", ref inputType, Type.GetType, false, "The CLR type name of the input.");
+
+                if(command == Commands.Schematics)
+                    if(schematicName == string.Empty)
+                        syntax.ReportError("name is a required parameter for schematics");
+            });
+
+            REstateHost.Agent.Configuration.RegisterComponent(new GrpcRemoteHostComponent(new GrpcHostOptions
+            {
+                Channel = new Channel(remoteEndoint, ChannelCredentials.Insecure),
+                UseAsDefaultEngine = true
+            }));
+
+
+            var yamlSerializer = new YamlDotNet.Serialization.Serializer();
+
+            if (command == Commands.Schematics)
+            {
+                Console.WriteLine(yamlSerializer.Serialize(
+                    REstateHost.Agent.GetStateEngine<string, string>()
+                        .GetSchematicAsync(schematicName, CancellationToken.None)));
+            }
+            
+        }
+    }
+
+    public enum Commands
+    {
+        None,
+        Schematics,
+        Machines
+    }
+}
