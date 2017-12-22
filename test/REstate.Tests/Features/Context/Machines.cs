4da5dfe7259ba44633cc11353510303234b0a4b8
Ovan Crone <ovan@psibr.net>
2017-12-22 13:34:41 -0800

    :white_check_mark: Add tests for Machine creation
    - Machine can be created from a Schematic

---
+++ test/REstate.Tests/Features/Context/Machines.cs
@@ -0,0 +1,25 @@
+﻿using REstate.Engine;
+using REstate.Schematics;
+using Xunit;
+
+namespace REstate.Tests.Features.Context
+{
+    public partial class REstateContext<TState, TInput>
+    {
+        public IStateMachine<TState, TInput> CurrentMachine { get; set; }
+
+        public void When_a_Machine_is_created_from_a_Schematic(Schematic<TState, TInput> schematic)
+        {
+            CurrentMachine = CurrentHost.Agent()
+                .GetStateEngine<TState, TInput>()
+                .CreateMachineAsync(schematic).GetAwaiter().GetResult();
+        }
+
+        public void Then_the_Machine_is_created_successfully(IStateMachine<TState, TInput> machine)
+        {
+            Assert.NotNull(machine);
+            Assert.NotNull(machine.MachineId);
+            Assert.NotEmpty(machine.MachineId);
+        }
+    }
+}
---
+++ test/REstate.Tests/Features/MachineCreation.cs
@@ -0,0 +1,53 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios.Contextual;
+using LightBDD.Framework.Scenarios.Extended;
+using LightBDD.XUnit2;
+using REstate.Tests.Features.Context;
+using Xunit.Abstractions;
+
+// ReSharper disable InconsistentNaming
+
+namespace REstate.Tests.Features
+{
+    [FeatureDescription(@"
+In order to utilize REstate's functionality
+As a developer
+I want to create machines from schematics")]
+    [ScenarioCategory("Machine Creation")]
+    public class MachineCreation
+        : FeatureFixture
+    {
+
+        [Scenario]
+        public void A_simple_machine_can_be_created()
+        {
+            Runner.WithContext<REstateContext<string, string>>().RunScenario(
+                _ => _.Given_a_new_host(),
+                _ => _.Given_a_simple_schematic_with_an_initial_state_INITIALSTATE("Initial"),
+                _ => _.When_a_Machine_is_created_from_a_Schematic(_.CurrentSchematic),
+                _ => _.Then_the_Machine_is_created_successfully(_.CurrentMachine));
+        }
+
+        // Given a default host
+        // And Given a schematic
+        // When I create a Machine
+        // Then the Machine is not null
+        // Then the Machine has an Id
+
+        // Given a default host
+        // And Given a schematic
+        // When I create a Machine with an Id
+        // Then the Machine is not null
+        // Then the Machine has the same Id
+
+        #region Constructor
+        public MachineCreation(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+        #endregion
+    }
+
+
+}
+
