faed673301ea07c3b95152491ffcdab67fc66776
Ovan Crone <ovan@psibr.net>
2017-12-23 06:22:13 -0800

    :white_check_mark: Add test for ToString implementations
    - Add test for Machine.ToString()
    - Add test for Schematic.ToString()

---
+++ test/REstate.Tests/Units/SchematicTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using REstate.Engine;
+using Xunit;
+
+namespace REstate.Tests.Units
+{
+    public class SchematicTests
+    {
+        [Fact]
+        public void Schematic_has_correct_ToString_functionality()
+        {
+            // Arrange
+            var schematic = REstateHost.Agent
+                .CreateSchematic<string, string>("Schematic")
+                .WithState("Initial", state => state
+                    .AsInitialState())
+                .Build();
+
+            // Act
+            var toString = schematic.ToString();
+
+            // Assert
+            Assert.NotNull(toString);
+            Assert.Equal(DotGraphCartographer<string, string>.Instance.WriteMap(schematic.States), toString);
+        }
+    }
+}
