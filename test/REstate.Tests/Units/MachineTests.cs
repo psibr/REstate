e1394899610d1f6bc77e96b80dd87b1154c24798
Ovan Crone <ovan@psibr.net>
2017-12-23 06:22:13 -0800

    :white_check_mark: Add test for ToString implementations
    - Add test for Machine.ToString()
    - Add test for Schematic.ToString()

---
+++ test/REstate.Tests/Units/MachineTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using REstate.Schematics;
+using Xunit;
+
+namespace REstate.Tests.Units
+{
+    public class MachineTests
+    {
+        [Fact]
+        public void Machine_has_correct_ToString_functionality()
+        {
+            // Arrange
+            var schematic = REstateHost.Agent
+                .CreateSchematic<string, string>("TestSchematic")
+                .WithState("Initial", state => state
+                    .AsInitialState())
+                .Build();
+
+            // Act
+            var machine = REstateHost.Agent
+                .GetStateEngine<string, string>()
+                .CreateMachineAsync(schematic).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.StartsWith($"{schematic.SchematicName}/", machine.ToString(), StringComparison.Ordinal);
+            Assert.EndsWith(machine.MachineId, machine.ToString(), StringComparison.Ordinal);
+        }
+    }
+}
