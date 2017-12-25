ffe72ba579906de846d7735ca62ca4ec8fd3bb78
Ovan Crone <ovan@psibr.net>
2017-12-25 08:11:54 -0800

    :white_check_mark: Add tests for Machine Creation with SchematicName
    - Change some Givens to support concurrent tests

--- test/REstate.Tests/Features/Context/Schematics.cs
+++ test/REstate.Tests/Features/Context/Schematics.cs
@@ -8,13 +8,20 @@ namespace REstate.Tests.Features.Context
     {
         public Schematic<TState, TInput> CurrentSchematic { get; set; }
 
-        public void Given_a_simple_schematic_with_an_initial_state_INITIALSTATE(TState initialState)
+        public void Given_a_Schematic_with_an_initial_state_INITIALSTATE(string schematicName, TState initialState)
         {
             CurrentSchematic = CurrentHost.Agent()
-                .CreateSchematic<TState, TInput>("simple")
+                .CreateSchematic<TState, TInput>(schematicName)
                 .WithState(initialState, state => state
                     .AsInitialState())
                 .Build();
+        }
+
+        public void Given_a_Schematic_is_stored(Schematic<TState, TInput> schematic)
+        {
+            CurrentHost.Agent()
+                .GetStateEngine<TState, TInput>()
+                .StoreSchematicAsync(schematic).GetAwaiter().GetResult();
         }
     }
 }
--- test/REstate.Tests/Features/MachineCreation.cs
+++ test/REstate.Tests/Features/MachineCreation.cs
@@ -1,4 +1,5 @@
-﻿using LightBDD.Framework;
+﻿using System.Reflection;
+using LightBDD.Framework;
 using LightBDD.Framework.Scenarios.Contextual;
 using LightBDD.Framework.Scenarios.Extended;
 using LightBDD.XUnit2;
@@ -17,28 +18,58 @@ I want to create machines from schematics")]
     public class MachineCreation
         : FeatureFixture
     {
-
         [Scenario]
         public void A_machine_can_be_created_from_a_Schematic()
         {
+            var schematicName = MethodBase.GetCurrentMethod().Name;
+
             Runner.WithContext<REstateContext<string, string>>().RunScenario(
                 _ => _.Given_a_new_host(),
-                _ => _.Given_a_simple_schematic_with_an_initial_state_INITIALSTATE("Initial"),
+                _ => _.Given_a_Schematic_with_an_initial_state_INITIALSTATE(schematicName, "Initial"),
                 _ => _.When_a_Machine_is_created_from_a_Schematic(_.CurrentSchematic),
+                _ => _.Then_the_Machine_is_created_successfully(_.CurrentMachine));
+        }
+
+        [Scenario]
+        public void A_machine_can_be_created_from_a_SchematicName()
+        {
+            var schematicName = MethodBase.GetCurrentMethod().Name;
+
+            Runner.WithContext<REstateContext<string, string>>().RunScenario(
+                _ => _.Given_a_new_host(),
+                _ => _.Given_a_Schematic_with_an_initial_state_INITIALSTATE(schematicName, "Initial"),
+                _ => _.Given_a_Schematic_is_stored(_.CurrentSchematic),
+                _ => _.When_a_Machine_is_created_from_a_SchematicName(_.CurrentSchematic.SchematicName),
                 _ => _.Then_the_Machine_is_created_successfully(_.CurrentMachine));
         }
 
         [Scenario]
         public void A_machine_can_be_created_from_a_Schematic_with_a_predefined_MachineId()
         {
-            var machineId = "12345";
+            var schematicName = MethodBase.GetCurrentMethod().Name;
+            var machineId = MethodBase.GetCurrentMethod().Name;
 
             Runner.WithContext<REstateContext<string, string>>().RunScenario(
                 _ => _.Given_a_new_host(),
-                _ => _.Given_a_simple_schematic_with_an_initial_state_INITIALSTATE("Initial"),
+                _ => _.Given_a_Schematic_with_an_initial_state_INITIALSTATE(schematicName, "Initial"),
                 _ => _.When_a_Machine_is_created_from_a_Schematic_with_a_predefined_MachineId(_.CurrentSchematic, machineId),
                 _ => _.Then_the_Machine_is_created_successfully(_.CurrentMachine),
                 _=> _.Then_the_MachineId_is_MACHINEID(_.CurrentMachine, machineId));
+        }
+
+        [Scenario]
+        public void A_machine_can_be_created_from_a_SchematicName_with_a_predefined_MachineId()
+        {
+            var schematicName = MethodBase.GetCurrentMethod().Name;
+            var machineId = MethodBase.GetCurrentMethod().Name;
+
+            Runner.WithContext<REstateContext<string, string>>().RunScenario(
+                _ => _.Given_a_new_host(),
+                _ => _.Given_a_Schematic_with_an_initial_state_INITIALSTATE(schematicName, "Initial"),
+                _ => _.Given_a_Schematic_is_stored(_.CurrentSchematic),
+                _ => _.When_a_Machine_is_created_from_a_SchematicName_with_a_predefined_MachineId(_.CurrentSchematic.SchematicName, machineId),
+                _ => _.Then_the_Machine_is_created_successfully(_.CurrentMachine),
+                _ => _.Then_the_MachineId_is_MACHINEID(_.CurrentMachine, machineId));
         }
 
         #region Constructor
