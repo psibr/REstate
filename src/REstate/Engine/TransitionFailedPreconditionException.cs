3c853400be3bafea149e8e3a1e9bd6347df26a28
Kyl Wellman <kyl.wellman@skykick.com>
2018-04-05 15:46:19 -0600

    Add strongly-typed exceptions for missing transitions and transitions that fail a precondition

---
+++ src/REstate/Engine/TransitionFailedPreconditionException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using REstate.Engine.Connectors;
+using REstate.Schematics;
+
+namespace REstate.Engine
+{
+    public class TransitionFailedPreconditionException<TState, TInput, TPayload>
+        : Exception
+    {
+        public Status<TState> FromState { get; }
+        public ITransition<TState, TInput> FailedTransition { get; }
+        public InputParameters<TInput, TPayload> InputParameters { get; }
+
+        public TransitionFailedPreconditionException(Status<TState> fromState,
+            ITransition<TState, TInput> transition,
+            InputParameters<TInput, TPayload> inputParameters)
+            : this(
+                  fromState, 
+                  transition, 
+                  inputParameters, 
+                  $"Transition from  {transition.Input} to {transition.ResultantState} failed while validating precondition")
+        { }
+
+        public TransitionFailedPreconditionException(Status<TState> fromState,
+            ITransition<TState, TInput> transition,
+            InputParameters<TInput, TPayload> inputParameters,
+            string message)
+            : this(
+                  fromState,
+                  transition,
+                  inputParameters,
+                  message,
+                  default)
+        { }
+
+        public TransitionFailedPreconditionException(Status<TState> fromState,
+            ITransition<TState, TInput> transition,
+            InputParameters<TInput, TPayload> inputParameters,
+            string message,
+            Exception innerException)
+            : base(message, innerException)
+        {
+            FromState = fromState;
+            FailedTransition = transition;
+            InputParameters = inputParameters;
+        }
+    }
+}
