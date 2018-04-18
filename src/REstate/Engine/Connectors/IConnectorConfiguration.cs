bf83801412a95deabb368d363adb96c28edf90f5
Ovan Crone <ovan.crone@skykick.com>
2018-04-17 18:50:53 -0700

    :book: Add documentation where missing for connectors.

--- src/REstate/Engine/Connectors/IConnectorConfiguration.cs
+++ src/REstate/Engine/Connectors/IConnectorConfiguration.cs
@@ -3,12 +3,14 @@
 namespace REstate.Engine.Connectors
 {
     /// <summary>
-    /// A configuration object that can be associated with a connector.
+    /// A configuration object that can be associated with a <see cref="IConnector"/>.
     /// </summary>
     public interface IConnectorConfiguration
     {
         /// <summary>
-        /// A resolution string that can be used as a <see cref="ConnectorKey"/> in a Schematic.
+        /// A resolution string that can be used as a <see cref="ConnectorKey"/> in a <see cref="Schematic{TState,TInput}"/>.
+        /// <para />
+        /// Identifiers should ALWAYS be unique, overriding is NOT supported for safety/clarity reasons.
         /// </summary>
         /// <example>
         /// log info
