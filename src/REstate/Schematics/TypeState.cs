81c8e23014e281b64d86539c5e265f2efa1913c8
Adrian Balfour <balfour@psibr.net>
2018-04-25 00:22:09 -0700

    - Add a Typed Schematic Builder - Add TypeState to support type checks in TypedSchematicBuilder

---
+++ src/REstate/Schematics/TypeState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+using REstate.Engine.Connectors;
+
+namespace REstate.Schematics
+{
+    /// <summary>
+    /// Represents a Type used as a State in a Schematic or Machine.
+    /// </summary>
+    public class TypeState : IEquatable<TypeState>
+    {
+        private string _assemblyQualifiedName;
+        private Lazy<Type> _underlyingType;
+        private Lazy<bool> _isActionable;
+        private Lazy<bool> _isPrecondition;
+
+        public string AssemblyQualifiedName
+        {
+            get => _assemblyQualifiedName;
+            set
+            {
+                _assemblyQualifiedName = value;
+
+                _underlyingType = new Lazy<Type>(GetUnderlyingType);
+                _isActionable = new Lazy<bool>(CheckIsActionable);
+                _isPrecondition = new Lazy<bool>(CheckIsPrecondition);
+            }
+        }
+
+        public bool AcceptsInputOf<TInput>() => 
+            (!IsActionable()
+                || typeof(IAction<TypeState, TInput>).IsAssignableFrom(_underlyingType.Value))
+            && (!IsPrecondition()
+                || typeof(IPrecondition).IsAssignableFrom(_underlyingType.Value));
+
+        public bool IsActionable() => _isActionable.Value;
+
+        public bool IsPrecondition() => _isPrecondition.Value;
+
+        public Type ToType() => _underlyingType.Value;
+
+        private Type GetUnderlyingType()
+            => Type.GetType(_assemblyQualifiedName);
+
+        private bool CheckIsActionable()
+            => typeof(IAction).IsAssignableFrom(_underlyingType.Value);
+
+        private bool CheckIsPrecondition()
+            => typeof(IPrecondition).IsAssignableFrom(_underlyingType.Value);
+
+        public bool Equals(TypeState other)
+        {
+            return this._assemblyQualifiedName == other._assemblyQualifiedName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is TypeState typeState)
+            {
+                return Equals(typeState);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this._assemblyQualifiedName.GetHashCode();
+        }
+
+        public static implicit operator TypeState(Type type)
+            => new TypeState { AssemblyQualifiedName = type.AssemblyQualifiedName };
+
+        public static implicit operator Type(TypeState typeState)
+            => typeState.ToType();
+    }
+}
---
+++ src/REstate/Schematics/TypedBuilder/TypedSchematicBuilder.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace REstate.Schematics.TypedBuilder
+{
+    class TypedSchematicBuilder<TInput> : ITypedSchematicBuilder<TInput>
+    {
+        public ICreationContext<TInput> StartsIn<TState>()
+        {
+            var ctx = new CreationContext<TInput>();
+
+            ctx.StartsIn<TState>();
+
+            return ctx;
+        }
+    }
+
+    class CreationContext<TInput> : ICreationContext<TInput>
+    {
+        internal Dictionary<Type, State<TypeState, TInput>> states = new Dictionary<Type, State<TypeState, TInput>>();
+
+        internal TypeState initialState;
+
+        internal Schematic<TypeState, TInput> schematic = new Schematic<TypeState, TInput>();
+
+        internal void StartsIn<TState>()
+        {
+            TypeState typeState = typeof(TState);
+
+            if (!typeState.AcceptsInputOf<TInput>())
+            {
+                throw new ArgumentException($"State {typeState.AssemblyQualifiedName} does not accept valid input for this machine.");
+            }
+
+            var state = new State<TypeState, TInput>
+            {
+                Value = typeof(TState)
+            };
+
+            states.Add(typeof(TState), state);
+
+            schematic.InitialState = typeof(TState);
+        }
+
+        public Schematic<TypeState, TInput> BuildAs(string schematicName)
+        {
+            schematic.SchematicName = schematicName;
+
+            schematic.States = states.Values.ToArray();
+
+            return schematic.Clone();
+        }
+
+        public IForStateContext<TState, TInput> For<TState>()
+        {
+            if (!states.TryGetValue(typeof(TState), out var state))
+            {
+                TypeState typeState = typeof(TState);
+
+                if (!typeState.AcceptsInputOf<TInput>())
+                {
+                    throw new ArgumentException($"State {typeState.AssemblyQualifiedName} does not accept valid input for this machine.");
+                }
+
+                state = new State<TypeState, TInput>
+                {
+                    Value = typeof(TState)
+                };
+
+                states.Add(typeof(TState), state);
+            }
+
+            return new ForStateContext<TState, TInput>() { creationContext = this, state = state };
+        }
+    }
+
+    class ForStateContext<TState, TInput> : IForStateContext<TState, TInput>
+    {
+        internal CreationContext<TInput> creationContext;
+
+        internal State<TypeState, TInput> state;
+
+        public IOnContext<TState, TInput> On(TInput input)
+        {
+            return new OnContext<TState, TInput> { creationContext = creationContext, state = state, input = input };
+        }
+    }
+
+    class OnContext<TState, TInput> : IOnContext<TState, TInput>
+    {
+        internal CreationContext<TInput> creationContext;
+
+        internal State<TypeState, TInput> state;
+
+        internal TInput input;
+
+        public ICreationContext<TInput> MoveTo<TNewState>()
+        {
+            if (!creationContext.states.TryGetValue(typeof(TNewState), out var state))
+            {
+                TypeState newTypeState = typeof(TNewState);
+
+                if (!newTypeState.AcceptsInputOf<TInput>())
+                {
+                    throw new ArgumentException($"State {newTypeState.AssemblyQualifiedName} does not accept valid input for this machine.");
+                }
+
+                state = new State<TypeState, TInput>
+                {
+                    Value = typeof(TNewState)
+                };
+
+                creationContext.states.Add(typeof(TNewState), state);
+            }
+
+            state.Transitions = state.Transitions.Concat(new[] 
+            {
+                new Transition<TypeState, TInput>
+                {
+                    Input = input,
+                    ResultantState = typeof(TNewState)
+                }
+            }).ToArray();
+
+            return creationContext;
+        }
+
+        public IWhenContext<TState, TInput> When<TPreconddition>()
+        {
+            TypeState preconditionTypeState = typeof(TPreconddition);
+
+            if (!preconditionTypeState.IsPrecondition())
+            {
+                throw new ArgumentException($"Type {preconditionTypeState.AssemblyQualifiedName} does not implement {nameof(Engine.Connectors.IPrecondition)}.");
+            }
+
+            return new WhenContext<TState, TInput>
+            {
+                creationContext = creationContext,
+                state = state,
+                input = input,
+                precondition = new Precondition
+                {
+                    ConnectorKey = preconditionTypeState.AssemblyQualifiedName
+                }
+            };
+        }
+    }
+
+    class WhenContext<TState, TInput> : IWhenContext<TState, TInput>
+    {
+        internal CreationContext<TInput> creationContext;
+
+        internal State<TypeState, TInput> state;
+
+        internal TInput input;
+
+        internal Precondition precondition;
+
+        public ICreationContext<TInput> MoveTo<TNewState>()
+        {
+            if (!creationContext.states.TryGetValue(typeof(TNewState), out var state))
+            {
+                TypeState newTypeState = typeof(TNewState);
+
+                if (!newTypeState.AcceptsInputOf<TInput>())
+                {
+                    throw new ArgumentException($"State {newTypeState.AssemblyQualifiedName} does not accept valid input for this machine.");
+                }
+
+                state = new State<TypeState, TInput>
+                {
+                    Value = typeof(TNewState)
+                };
+
+                creationContext.states.Add(typeof(TNewState), state);
+            }
+
+            state.Transitions = state.Transitions.Concat(
+                new[]
+                {
+                    new Transition<TypeState, TInput>
+                    {
+                        Input = input,
+                        ResultantState = typeof(TNewState),
+                        Precondition = precondition
+                    }
+                }).ToArray();
+
+            return creationContext;
+        }
+    }
+}
