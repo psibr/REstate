17d206a211163f95707a907706b43d18b5f912d7
Ovan Crone <ovan@psibr.net>
2018-11-04 22:44:41 -0800

    Add State representation extensions to help with queries

---
+++ src/REstate.Engine.Repositories.EntityFrameworkCore/StateRepresentationsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MessagePack;
+using MessagePack.Resolvers;
+
+namespace REstate.Engine.Repositories.EntityFrameworkCore
+{
+    public static class StateRepresentationsExtensions
+    {
+        public static string ToStateRepresentation<TState>(TState state)
+        {
+            return MessagePackSerializer.ToJson(state, ContractlessStandardResolver.Instance);
+        }
+
+        public static TState ToState<TState>(string stateRepresentation)
+        {
+            return MessagePackSerializer.Deserialize<TState>(
+                MessagePackSerializer.FromJson(stateRepresentation),
+                ContractlessStandardResolver.Instance);
+        }
+    }
+}
