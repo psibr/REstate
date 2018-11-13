aa1830c820aa060d654d9885c934e29941c6611e
Ovan Crone <ovan@psibr.net>
2018-11-13 03:25:44 -0800

    Remove runtime restrictions of migrations

--- src/REstate.EntityFrameworkCore.Migrations.SqlServer/DesignTimeREstateDbContextFactory.cs
+++ src/REstate.EntityFrameworkCore.Migrations.SqlServer/DesignTimeREstateDbContextFactory.cs
@@ -2,12 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using REstate.Engine.Repositories.EntityFrameworkCore;
 
-namespace REstate.EntityFrameworkCore.Migrations
+namespace REstate.EntityFrameworkCore.Migrations.SqlServer
 {
     /// <summary>
     /// This is used to generate migrations. NOT intended to be used by consumers.
     /// </summary>
-    internal class DesignTimeREstateDbContext : IDesignTimeDbContextFactory<REstateDbContext>
+    internal class DesignTimeREstateDbContextFactory : IDesignTimeDbContextFactory<REstateDbContext>
     {
         public REstateDbContext CreateDbContext(string[] args)
         {
