46c2bd542dc3cb2cbe8378d48a7a7f898c5319c6
Zoey McCullough <zoey.c.mccullough@gmail.com>
2020-01-21 19:56:04 -0800

    Added redis test project and context - Added feature tests for machine manipulation

---
+++ test/REState.Engine.Repositories.Redis.Tests/Features/MachineRetrieval.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using LightBDD.Framework;
+using LightBDD.Framework.Scenarios;
+using REstate.Engine.Repositories.Redis.Tests.Features.Context;
+using REstate.Tests.Features;
+
+// ReSharper disable InconsistentNaming
+
+namespace REstate.Engine.Repositories.Redis.Tests.Features
+{
+    [FeatureDescription(@"
+In order to use familiar storage
+As a developer
+I want to retrieve previously created Machines stored using Redis")]
+    [ScenarioCategory("Machine Retrieval")]
+    [ScenarioCategory("Redis")]
+    public class MachineRetrieval
+        : MachineRetrievalScenarios<REstateRedisContext<string, string>>
+    {        
+        protected override Task<CompositeStep> Given_host_configuration_is_applied()
+        {
+            return Task.FromResult(
+                CompositeStep
+                    .DefineNew()
+                    .WithContext(Context)
+                    .AddAsyncSteps(
+                        _ => _.Given_a_new_host(),
+                        _ => _.Given_Redis_is_the_registered_repository())
+                    .Build());
+        }
+    }
+}
