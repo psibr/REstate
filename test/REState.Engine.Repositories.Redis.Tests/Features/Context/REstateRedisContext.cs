2e88ee85883f12176c9f3fb652255e6aa85ee20a
Zoey McCullough <zoey.c.mccullough@gmail.com>
2020-01-21 21:43:28 -0800

    Add redis tests (#68)
    * Added redis test project and context
    
    *  Added feature tests for machine manipulation
    
    * Updated repository to use message pack serializer options
    
    * Fixed commit number bug
    
    * Add adding an action to start Redis
    
    * Add Windows category to the provider tests
    
    * Switch to Ubuntu and filter Windows test cases
    
    Co-authored-by: Aida <aida@psibr.net>

---
+++ test/REState.Engine.Repositories.Redis.Tests/Features/Context/REstateRedisContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using REstate.Tests.Features.Context;
+using StackExchange.Redis;
+
+namespace REstate.Engine.Repositories.Redis.Tests.Features.Context
+{
+    public class REstateRedisContext<TState, TInput>
+        : REstateContext<TState, TInput>
+    {
+        #region GIVEN
+        public Task Given_Redis_is_the_registered_repository()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+            var redisDatabase = redis.GetDatabase();
+
+            CurrentHost.Agent().Configuration
+                .RegisterComponent(new RedisRepositoryComponent(
+                    redisDatabase));
+
+            return Task.CompletedTask;
+        }
+        #endregion
+
+        #region WHEN
+
+        #endregion
+
+        #region THEN
+
+        #endregion
+    }
+}
