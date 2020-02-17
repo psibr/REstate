f8d0963e72bc4746db0014723dd87689c7c2b658
Aida Crone <aida@psibr.net>
2020-02-16 16:51:51 -0800

    Add XML docs to Schematics

--- src/REstate/Schematics/Schematic.cs
+++ src/REstate/Schematics/Schematic.cs
@@ -6,6 +6,11 @@ using REstate.Engine;
 
 namespace REstate.Schematics
 {
+    /// <summary>
+    /// Mutable object format for Schematics, a definition of a state machine.
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <typeparam name="TInput"></typeparam>
     public class Schematic<TState, TInput> 
         : ISchematic<TState, TInput>
     {
@@ -24,16 +29,31 @@ namespace REstate.Schematics
         [Required]
         public string SchematicName { get; set; }
 
+        /// <summary>
+        /// The initial state of the machine when created.
+        /// </summary>
         [Required]
         public TState InitialState { get; set; }
 
+        /// <summary>
+        /// Allows the machine to retry an update when a <see cref="StateConflictException"/> occurs.
+        /// </summary>
         public int StateConflictRetryCount { get; set; }
 
-        IReadOnlyDictionary<TState, IState<TState, TInput>> ISchematic<TState, TInput>.States => 
+        /// <inheritdoc />
+        IReadOnlyDictionary<TState, IState<TState, TInput>> ISchematic<TState, TInput>.States =>
             States.ToDictionary(
                 c => c.Value,
                 c => (IState<TState, TInput>)c);
 
+        /// <summary>
+        /// States that are available to the machine.
+        /// 
+        /// <para>
+        ///     States may also declare transitions, actions, preconditions, and substates.
+        /// </para>
+        /// </summary>
+        /// <remarks>Used for serialization and interchange primarily. See <see cref="ISchematic&lt;TState, TInput&gt;.States"/> for the readonly view.</remarks>
         [Required]
         public State<TState, TInput>[] States { get; set; }
 
